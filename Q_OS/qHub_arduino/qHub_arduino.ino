8814a675f98cf0fa295726fa80483774b4bd4a84
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-23 21:16:25 +0200

    update

--- Q_OS/qHub_arduino/qHub_arduino.ino
+++ Q_OS/qHub_arduino/qHub_arduino.ino
@@ -1,11 +1,9 @@
 //Arduino sketch for QHub built in Q OS by Anspirit co.
 
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
--- Q_OS/server/index.js
+++ Q_OS/server/index.js
@@ -23,7 +23,36 @@ app.post('/hub', function(req, res){
         var responseToSend = {access: true, error: null};
         res.send(JSON.stringify(responseToSend));
         console.log(task);
-        //TODO process task
+        //TODO get device to manipulate
+      }else{
+        var responseToSend = {access: false, error: 'no access'};
+        res.send(JSON.stringify(responseToSend));
+      }
+    });
+  }else{
+      res.send(JSON.stringify({error: true, type: 'bad request'}));
+  }
+});
+app.post('/deviceSet', function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  var secret = req.body.secret;
+  var user = req.body.user;
+  var device = req.body.device;
+  var state = req.body.state;
+
+  if(user != null && secret != null && device != null && state != null){
+    var security = require("./api/verifySecurity.js");
+    security.accessForUser(user, secret, function(access){
+      if(access){
+        var responseToSend = {access: true, error: null};
+        res.send(JSON.stringify(responseToSend));
+        console.log(task);
+        /*  TODO
+          1. Get device type from database
+          2. Find extension for needed device
+          3. Run it with state
+          4.
+        */
       }else{
         var responseToSend = {access: false, error: 'no access'};
         res.send(JSON.stringify(responseToSend));
